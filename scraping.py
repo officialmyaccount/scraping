3c34fa502499bb33a11eaa8e6527f526cb77716c
officialmyaccount <bicebc@gmail.com>
2023-07-06 18:42:46 +0900

    URLリストから記事URLを取得し記事内容を取得

--- scraping.py
+++ scraping.py
@@ -8,6 +8,17 @@ rest = requests.get(URL)
 soup = BeautifulSoup(rest.text, "html.parser")
 
 data_list = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
+
+headline_link_list = [data.attrs["href"] for data in data_list]
+for headline_link in headline_link_list:
+    summary = requests.get(headline_link)
+    summary_soup = BeautifulSoup(summary.text, "html.parser")
+    summary_soup_a = summary_soup.select("a:contains('続きを読む')")[0]
+    
+    news_body_link = summary_soup_a.attrs["href"]
+    news_body = requests.get(news_body_link)
+'''
 for data in data_list:
     print(data.span.string)
     print(data.attrs["href"])
+'''
